e4318f91c2a0fbf50923f48f0f8d0d2db74b3121
Karmaicom Martins <karmaicom@gmail.com>
2023-02-17 20:20:47 -0300

    Projeto MVC

---
+++ MVC/Todo/Todo.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Web.Controllers
+{
+
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("hello")]
+        public string Hello()
+        {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ MVC/Todo/Todo.Web/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Web.Models;
+
+namespace Todo.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=app.db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    }
+}
